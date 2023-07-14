b9a8d32d0927ebf287830be84aea3d7dcc542a35
user <datndt@smartosc.com>
2023-07-14 16:47:38 +0700

    update folder redux

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import {NavigationContainer, Route} from '@react-navigation/native';
 import AppNavigator from './app/presentation/navigator/AppNavigator';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {Provider} from 'react-redux';
+import store from './app/presentation/redux/Store/IndexStore.js';
 
 const App = () => {
   const [currentScreen, setCurrentScreen] = useState<string>('SplashScreen');
@@ -11,23 +13,25 @@ const App = () => {
   };
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          backgroundColor:
-            currentScreen === 'SplashScreen' ? '#FFFFFF' : '#376AED',
-        }}
-        edges={['top']}>
-        <NavigationContainer
-          onStateChange={(state: {routes: Route[]; index: number}): void => {
-            const currentRoute = state.routes[state.index].name;
-            handleScreenChange(currentRoute);
-          }}>
-          <AppNavigator />
-        </NavigationContainer>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <SafeAreaView
+          style={{
+            flex: 1,
+            backgroundColor:
+              currentScreen === 'SplashScreen' ? '#FFFFFF' : '#376AED',
+          }}
+          edges={['top']}>
+          <NavigationContainer
+            onStateChange={(state: {routes: Route[]; index: number}): void => {
+              const currentRoute = state.routes[state.index].name;
+              handleScreenChange(currentRoute);
+            }}>
+            <AppNavigator />
+          </NavigationContainer>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
